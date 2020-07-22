1a6e7198412a3921ea26554f5f74d92cd3eb2569
myusername <lutt.ravenx@gmail.com>
2020-07-21 23:27:29 -0300

    API NODE CRUD

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
+const cors = require('cors')
 const mongoose = require('mongoose');
 const requireDir = require('require-dir')
 
 // Start APP
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // Start Database
 mongoose.connect('mongodb://localhost:27017/nodeapi',
@@ -13,17 +15,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi',
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-//First route
-app.get('/',(req,res) => {
-    Product.create({ 
-        title: 'React Native',
-        description: ' Build Native Apps',
-        url: 'http://github.com/facebook/react-native'
-    });
-    
-    return res.send('Hello xx');
-});
+//Routes
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
